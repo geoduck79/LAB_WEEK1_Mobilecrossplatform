12a4d1b2e5ab4370adcb59c1c143e8f5bda840b8
davidlin17 <davidkedua7@gmail.com>
2026-02-18 11:09:56 +0700

    test

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>John Hubert Chen -00000094925.</Text>
+    </View>
+  );
+}
